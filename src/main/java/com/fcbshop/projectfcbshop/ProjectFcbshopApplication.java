abdcd871f51e234a7f265118e152368bf70b2327
yenkemiel <yenkemiel@gmail.com>
2023-05-27 18:10:25 +0800

    first commit

---
+++ src/main/java/com/fcbshop/projectfcbshop/ProjectFcbshopApplication.java
@@ -0,0 +1,13 @@
+package com.fcbshop.projectfcbshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFcbshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFcbshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fcbshop/projectfcbshop/ProjectFcbshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcbshop.projectfcbshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFcbshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
